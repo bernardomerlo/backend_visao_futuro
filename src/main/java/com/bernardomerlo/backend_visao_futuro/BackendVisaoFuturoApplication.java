f6f08a753f9545a1631cd1ef5e327b8b9f1c766a
bernardomerlo <bernardomerlo49@gmail.com>
2025-08-25 18:25:15 -0300

    build(init): primeiro commit

---
+++ src/main/java/com/bernardomerlo/backend_visao_futuro/BackendVisaoFuturoApplication.java
@@ -0,0 +1,13 @@
+package com.bernardomerlo.backend_visao_futuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVisaoFuturoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendVisaoFuturoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bernardomerlo/backend_visao_futuro/BackendVisaoFuturoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernardomerlo.backend_visao_futuro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVisaoFuturoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
